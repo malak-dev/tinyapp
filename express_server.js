77bb959622e353ad5321d62afb505b273243251c
malak <malak-y@live.com>
2019-12-09 22:26:44 +0000

    Create a URL Submission Form

--- express_server.js
+++ express_server.js
@@ -16,6 +16,9 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
